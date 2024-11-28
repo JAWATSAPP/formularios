4a1d60bda07b2f71bd99077df798c0d43a475a2b
Jose Antonio <flhorianj@gmail.com>
2024-11-27 21:58:22 -0500

    formulario

---
+++ src/main/java/com/example/sesion4/controler/ForController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sesion4.controler;
+
+
+import com.example.sesion4.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+/**
+ *
+ * @author FLHORIAN
+ */
+@Controller
+public class ForController {
+    
+    @GetMapping("/form")
+    public  String showForm(Model model) {
+        model.addAttribute("user",  new User());
+        return "form";
+        
+    }
+    @PostMapping("/submit")
+    public String submitForm(@ModelAttribute User user, Model model){
+        model.addAttribute("user", user);
+        return  "succes";
+    }
+            }
---
+++ src/main/java/com/example/sesion4/model/User.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sesion4.model;
+
+/**
+ *
+ * @author FLHORIAN
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
+
